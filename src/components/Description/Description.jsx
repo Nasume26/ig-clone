7701ec270f25e13ee4d4de132281bd5f79f77f4f
Maxwell Roberson <108200270+Nasume26@users.noreply.github.com>
2022-10-03 11:08:52 -0500

    Description essentially done

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -5,11 +5,11 @@ const Description = (props) => {
 
     return (
         <div className="description">
-            <p>What is up this is my, {profile.username}'s awesome mega cool radical
+            <p>What is up this is {profile.username}'s awesome mega cool radical
             profile. As you can see you can look at the same pictures any other user
             utilizing this application can access so you might say I am not 
             very unique. However the Dev also doesn't have several weeks to create a full
-            stack clone of IG so this is what you get.</p>
+            stack clone of IG so this is the best I can do.</p>
         </div>
     )
 }
