d0292f882c8dad2448c9584d180416cfeda2ab61
Maxwell Roberson <108200270+Nasume26@users.noreply.github.com>
2022-10-03 10:59:25 -0500

    Basic Profile Header finished

--- src/components/ProfileHeader/ProfileHeader.jsx
+++ src/components/ProfileHeader/ProfileHeader.jsx
@@ -1,5 +1,7 @@
 import "./ProfileHeader.scss"
 import {CgProfile} from "react-icons/cg"
+import { FiMoreHorizontal } from "react-icons/fi";
+import { MdKeyboardArrowDown } from "react-icons/md";
 
 const ProfileHeader = (props) => {
 const {profile} = props;
@@ -17,7 +19,14 @@ const {profile} = props;
                 <div className="profile-header__midsection__follow">
                     Follow
                 </div>
-
+            </div>
+            <div className="profile-header__end">
+                <div className="profile-header__end__more">
+                    <FiMoreHorizontal />
+                </div>
+                <div className="profile-header__end__select">
+                    <MdKeyboardArrowDown />
+                </div>
             </div>
         </div>
     )
