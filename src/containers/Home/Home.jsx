b731e8230ec3a29a5db7a6317880a4660cfabd2f
Maxwell Roberson <108200270+Nasume26@users.noreply.github.com>
2022-10-03 11:17:48 -0500

    Began workshopping Image rendering

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,14 +1,23 @@
 import Description from "../../components/Description/Description";
 import ProfileHeader from "../../components/ProfileHeader/ProfileHeader";
 import "./Home.scss"
+import corn1 from "../../images/corn1.jpeg"
+import corn2 from "../../images/Ears-corn.webp"
 
 const Home = (props) => {
-const {profile} = props;
+    const {profile} = props;
+
+    const imagesArr = [corn1, corn2]
+
+    const renderImages = imagesArr.map((image) => {
+        return <img src= {image} />
+    })
 
     return (
         <div className="home">
             <ProfileHeader profile = {profile}/>
             <Description profile = {profile}/>
+            {renderImages}
         </div>
     )
 }
