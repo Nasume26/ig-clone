bef5d8ee5c3ebac2b231103c7af3b5e794cfdfac
Maxwell Roberson <108200270+Nasume26@users.noreply.github.com>
2022-10-03 11:57:58 -0500

    Image select done

--- src/components/ImageSelect/ImageSelect.jsx
+++ src/components/ImageSelect/ImageSelect.jsx
@@ -1,11 +1,26 @@
 import "./ImageSelect.scss"
+import {BiGrid} from "react-icons/bi"
+import { BiMoviePlay } from "react-icons/bi"
+import { AiOutlinePlayCircle } from "react-icons/ai"
+import { AiOutlineTag } from "react-icons/ai"
 
 const ImageSelect = (props) => {
 
 
     return (
-        <div>
-
+        <div className="image-select">
+            <div className="image-select__grid">
+                <BiGrid />
+            </div>
+            <div className="image-select__movie">
+                <BiMoviePlay />
+            </div>
+            <div className="image-select__video">
+                <AiOutlinePlayCircle />
+            </div>
+            <div className="image-select__tagged">
+                <AiOutlineTag />
+            </div>
         </div>
     )
 }
